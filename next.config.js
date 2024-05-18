a3674ecc8ebe9d780651c7368b8b7604a9288ab6
Denis Chernov <chernoff.cs@gmail.com>
2024-05-18 19:24:24 +0300

    Updating

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Output the static files to somewhere more obvious
+  distDir: 'build',
+  experimental: {
+    // This is experimental but can
+    // be enabled to allow parallel threads
+    // with nextjs automatic static generation
+    workerThreads: false,
+    cpus: 4
+  }
+}
 
 module.exports = nextConfig
