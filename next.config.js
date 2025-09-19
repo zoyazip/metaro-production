a203ffbb044e4787b484b1ee4c73fdca802ab320
Denis Chernov <chernoff.cs@gmail.com>
2025-09-19 21:12:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   // Output the static files to somewhere more obvious
   distDir: 'build',
+  experimental: {
+    workerThreads: false,
+    cpus: 2
+  }
 }
 
 module.exports = nextConfig
