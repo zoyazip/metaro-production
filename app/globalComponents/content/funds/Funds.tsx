11176b47a989fcb5d738838f0242cd26e2245fcf
Denis Chernov <chernoff.cs@gmail.com>
2025-09-19 21:40:06 +0300

    Update Funds.tsx

--- app/globalComponents/content/funds/Funds.tsx
+++ app/globalComponents/content/funds/Funds.tsx
@@ -9,7 +9,7 @@ const Funds = () => {
                     <h5>Automatizētas optiskās šķiedras lāzergriešanas iekārtas ar programmatūru iegāde</h5>
                     <p>SIA “METĀRO” ir noslēdzis līgumu ar Akciju sabiedrību „Attīstības finanšu institūcija Altum” (ALTUM) par automatizētas optiskās šķiedras lāzergriešanas iekārtas ar programmatūru iegādi un uzstādīšanu. Projekta kopējās izmaksas: 475 000,00 EUR, bet Atveseļošanās fonda (AF) finansējums sastāda 166 250,00 EUR. </p>
                 </div>
-                <Image src='/footerInfo.png' alt='Nacionālais Attīstības Plāns 2020' width={400} height={104}/>
+                <Image src='/footerInfo.png' alt='Nacionālais Attīstības Plāns 2020' width={459} height={240} />
             </div>
         </section>
     )
