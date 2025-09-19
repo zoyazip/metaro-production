98ae015a1ff3195bb06a5cc78999d59402cef67d
Denis Chernov <chernoff.cs@gmail.com>
2025-09-19 21:55:01 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Output the static files to somewhere more obvious
-  distDir: 'build',
-  experimental: {
-    workerThreads: false,
-    cpus: 2
-  }
+  distDir: 'build'
 }
 
 module.exports = nextConfig
